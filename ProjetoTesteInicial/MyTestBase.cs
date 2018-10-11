97e5537dd0565d82da8839284664d88675d7a014
Anderson Mendes de Almeida <anderson.almeida@primecontrol.com.br>
2018-10-11 16:40:52 -0300

    primeiro update

---
+++ ProjetoTesteInicial/MyTestBase.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.Events;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoTesteInicial
+{
+    [TestClass]
+    public class MyTestBase
+    {
+        private static IWebDriver driver;
+
+        public void GoToUrl(string url)
+        {
+            driver.Navigate().GoToUrl(url);
+            driver.Manage().Window.Maximize();
+        }
+
+        public static By Imagens()
+        {
+            return By.PartialLinkText("Imagens");
+        }
+
+        public static By BuscarImagens()
+        {
+            return By.Id("lst-ib");
+        }
+
+        [TestInitialize]
+        public void MinhaInicializacao()
+        {
+            driver = new ChromeDriver(Path.Combine(AppDomain.CurrentDomain.BaseDirectory));
+        }
+        
+        [TestMethod]
+        public void PrimeiroTeste()
+        {
+            string url = "https://www.google.com.br";
+            GoToUrl(url);
+
+            driver.FindElement(Imagens()).Click();
+            driver.FindElement(BuscarImagens()).SendKeys("teste" + Keys.Return);
+        }
+    }
+}
---
+++ ProjetoTesteInicial/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProjetoTesteInicial")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProjetoTesteInicial")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5d1f71eb-f862-4924-9d35-4e92a891c401")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
